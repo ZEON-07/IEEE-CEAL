52d7893b9c4dae114fc32aaa1ffe68db51581b1b
zyndor1548 <a.s.aromal2006@gmail.com>
2025-06-07 01:00:21 +0530

    fixed logo in  nav bar not loading as the photos MUST be loaded from root directory else it will not load in pages which are in folders

--- loader.js
+++ loader.js
@@ -1,13 +1,13 @@
+const API_URL = 'https://script.google.com/macros/s/AKfycbzwYBil0NmjIJeXtrDOQbmEZiqm7yJGtCcsxas-e4DwMYEHrpsS_r3RFZToJ8bwr5WwmA/exec';
 document.addEventListener("DOMContentLoaded", () => {
   // Load navbar from site root
-  fetch("./nav-bar.html")
+  fetch("/nav-bar.html")
     .then((res) => res.text())
     .then((data) => {
       document.getElementById("NavBar").innerHTML = data;
 
       const navContainer = document.querySelector(".nav-container");
       const scrollThreshold = 50;
-
       window.addEventListener("scroll", () => {
         if (window.scrollY > scrollThreshold) {
           navContainer.classList.add("scrolled");
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
   // Load footer from site root
-  fetch("./footer.html")
+  fetch("/footer.html")
     .then((res) => res.text())
     .then((data) => {
       document.getElementById("Footer").innerHTML = data;
@@ -28,4 +28,32 @@ document.addEventListener("DOMContentLoaded", () => {
         yearSpan.textContent = new Date().getFullYear();
       }
     });
+  // to load list in navbar
+  FetchAllYears();
 });
+async function FetchAllYears() {
+  let res = await fetch(`${API_URL}?allyears`);
+  let data = await res.json();
+  if (!data || !data.allyears) {
+    return;
+  }
+  AddOptions(data.allyears)
+}
+function AddOptions(years) {
+  const Dropdown = document.getElementById('execom_dropdown');
+  Dropdown.innerHTML = '';
+  if (years == null) {
+    const list = document.createElement('li');
+    list.innerHTML = `No Execom loaded`;
+    Dropdown.appendChild(list);
+  }
+  years.forEach(year => {
+    const list = document.createElement('li');
+    list.innerHTML = `Execom ${year}`;
+    list.onclick = () => {
+      window.location.href = `execom/index.html?year=${year}`;
+    };
+    Dropdown.appendChild(list);
+  });
+}
+
